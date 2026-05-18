ac2354ec67cce08b85384c4ad7be94957df7446c
Yuji <elfahkrany.yuji@muse-gr.com>
2026-05-18 19:16:52 +0900

    api handling

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
-const app = require('../backend/app');
+let app;
+
+try {
+  app = require('../backend/app');
+} catch (err) {
+  app = (req, res) => {
+    res.status(500).json({
+      error: 'Failed to load app',
+      message: err.message
+    });
+  };
+}
 
 module.exports = app;
